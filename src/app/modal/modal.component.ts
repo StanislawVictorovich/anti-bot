76ade82c3c61db05765649f023e6a6ff67a607d7
Stanislav Svirepov <svirepov.stanislav@gmail.com>
2018-11-21 14:43:57 +0200

    adding interface & ficing bugs

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -3,6 +3,15 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { ModalService } from '../modal.service';
 
+
+interface UserData {
+  login: string;
+  email: string;
+  text: string;
+  result: number;
+}
+
+
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
@@ -12,12 +21,17 @@ export class ModalComponent {
   
   public modalRef: BsModalRef;
 
-  public userData = {};
+  public userData: UserData = {
+    login: '',
+    email: '',
+    text: '',
+    result: 0
+  };
 
   constructor(private bsModalService: BsModalService, private modalService: ModalService) {
   }
  
-  public openModal(template: TemplateRef<any>) {
+  public openModal(template: TemplateRef<BsModalRef>) {
     this.modalRef = this.bsModalService.show(template);
     this.modalService.mixValues();
   }
